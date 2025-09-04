53bb474ed58083358804c6d376eb6587c961cdf7
negin-khademi <negin.khademi@gmail.com>
2025-09-04 17:08:39 +0330

    feat : install primeng icon

--- src/app/core/app.config.ts
+++ src/app/core/app.config.ts
@@ -4,11 +4,20 @@ import {
   provideZonelessChangeDetection,
 } from "@angular/core";
 
+import Aura from "@primeuix/themes/aura";
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
+import { providePrimeNG } from "primeng/config";
 import { provideRouter } from "@angular/router";
 import { routes } from "../app.routes";
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
