2d860eeb78574e786705bfce0ccca4f21cd0d41a
negin-khademi <negin.khademi@gmail.com>
2025-09-04 17:48:25 +0330

    feat : design header part in mobile mode

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { ButtonModule } from "primeng/button";
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  imports: [],
-  templateUrl: './header.html',
-  styleUrl: './header.scss'
+  selector: "app-header",
+  imports: [ButtonModule, CommonModule],
+  templateUrl: "./header.html",
+  styleUrl: "./header.scss",
 })
 export class Header {
+  isOpen = false;
+  searchText = "";
 
+  toggleSearch() {
+    this.isOpen = !this.isOpen;
+    if (!this.isOpen) {
+      this.searchText = "";
+    }
+  }
 }
