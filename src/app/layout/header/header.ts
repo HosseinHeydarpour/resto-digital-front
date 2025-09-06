dc6cc9832f984161226f80b1954f9663869dfd30
negin-khademi <negin.khademi@gmail.com>
2025-09-06 11:05:37 +0330

    feat(navbar): track and update active icon index on click

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -12,8 +12,21 @@ import menu from "./menu.json";
 export class Header {
   isOpen = false;
   searchText = "";
-
   menuItems: any[] = menu;
+  // Make first item active by default
+  activeIndex: number = 0;
+
+  /**
+   * Sets the active icon index.
+   *
+   * @param index - The index of the icon to mark as active.
+   *
+   * Updates the `activeIndex` property to track which icon is currently active.
+   * This is typically used to highlight the selected icon in the UI.
+   */
+  setActiveIconIndex(index: number) {
+    this.activeIndex = index;
+  }
 
   toggleSearch() {
     this.isOpen = !this.isOpen;
