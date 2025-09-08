db8629b495806af8ce311c4b08985150104dd516
negin-khademi <negin.khademi@gmail.com>
2025-09-08 11:19:44 +0330

    feat : add some colors and fontsize to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
       colors: {
         "dark-teal": "hsl(180, 44%, 17%)",
         "light-green": "hsl(104, 40%, 71%)",
+        "normal-gray": "hsl(182, 14%, 38%)",
         "light-gray": "hsl(0, 0%, 96%)",
+        "account-color": "#ffa600",
+      },
+      fontSize: {
+        xxs: "0.625rem", // use text-xxs
       },
     },
   },
