2ae1fb42f8eaf697fc49f75a44eb77a1b79c4d72
negin-khademi <negin.khademi@gmail.com>
2025-09-09 13:45:27 +0330

    feat : import MenuCards in home

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { Header } from "../../layout/header/header";
 import { Hero } from "./hero/hero";
+import { MenuCards } from "./menu-cards/menu-cards";
 
 @Component({
   selector: "app-home",
-  imports: [Header, Hero],
+  imports: [Header, Hero, MenuCards],
   templateUrl: "./home.html",
   styleUrl: "./home.scss",
 })
