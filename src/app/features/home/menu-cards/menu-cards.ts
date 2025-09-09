3e340a868cca62245a43076e7d85e4ff1325e3aa
negin-khademi <negin.khademi@gmail.com>
2025-09-09 12:41:02 +0330

    feat : generate menu cards component

---
+++ src/app/features/home/menu-cards/menu-cards.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-cards',
+  imports: [],
+  templateUrl: './menu-cards.html',
+  styleUrl: './menu-cards.scss'
+})
+export class MenuCards {
+
+}
