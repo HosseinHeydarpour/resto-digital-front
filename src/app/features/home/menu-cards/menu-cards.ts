62eadfa1c594d2fe623511aeaae73e66266fdafa
negin-khademi <negin.khademi@gmail.com>
2025-09-09 15:45:34 +0330

    feat : rendeer card component for showing list of food

--- src/app/features/home/menu-cards/menu-cards.ts
+++ src/app/features/home/menu-cards/menu-cards.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Card } from "../../../shared/components/card/card";
+import { Component } from "@angular/core";
+import { IFoodItem } from "../../../shared/models/IFoodList";
+import { ScrollPanelModule } from "primeng/scrollpanel";
+import foodList from "./food-list.json";
 
 @Component({
-  selector: 'app-menu-cards',
-  imports: [],
-  templateUrl: './menu-cards.html',
-  styleUrl: './menu-cards.scss'
+  selector: "app-menu-cards",
+  imports: [Card, ScrollPanelModule],
+  templateUrl: "./menu-cards.html",
+  styleUrl: "./menu-cards.scss",
 })
 export class MenuCards {
-
+  list: IFoodItem[] = foodList;
 }
