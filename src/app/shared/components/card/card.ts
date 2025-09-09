c761b33d8480c95064b7e1fa4e4d9acd6744f43e
negin-khademi <negin.khademi@gmail.com>
2025-09-09 15:44:52 +0330

    feat : style card component for using it in menu-cards component

--- src/app/shared/components/card/card.ts
+++ src/app/shared/components/card/card.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input, Signal, signal } from "@angular/core";
 
 @Component({
   selector: "app-card",
@@ -6,4 +6,17 @@ import { Component } from "@angular/core";
   templateUrl: "./card.html",
   styleUrl: "./card.scss",
 })
-export class Card {}
+export class Card {
+  @Input() set item(value: { name: string; imageUrl: string }) {
+    this._item.set(value);
+  }
+
+  // Internal signal
+  readonly _item = signal<{ name: string; imageUrl: string }>({
+    name: "",
+    imageUrl: "",
+  });
+
+  // You can expose it as a readonly signal for the template
+  readonly itemSignal: Signal<{ name: string; imageUrl: string }> = this._item;
+}
