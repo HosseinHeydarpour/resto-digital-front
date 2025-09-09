cdc151a9317da7ca64fbc0fbf17a13e162244d5e
negin-khademi <negin.khademi@gmail.com>
2025-09-09 12:22:21 +0330

    feat : use api in service

--- src/app/core/services/restrant-info.ts
+++ src/app/core/services/restrant-info.ts
@@ -1,8 +1,20 @@
+import { HttpClient } from "@angular/common/http";
+import { IRestaurant } from "../../shared/models/IRestaurantInfo";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment } from "../../../environments/environment";
 
 @Injectable({
   providedIn: "root",
 })
 export class RestrantInfo {
-  restaurant!: RestrantInfo[];
+  private apiUrl = `${environment.MAIN_SITE_URL}/api/v1/restaurants/60d5ec49f72e9a3d8c8a8a8a`;
+  restaurant!: IRestaurant[];
+
+  constructor(private http: HttpClient) {}
+
+  // get all information
+  getAllRestaurantInfo(): Observable<IRestaurant[]> {
+    return this.http.get<IRestaurant[]>(this.apiUrl);
+  }
 }
