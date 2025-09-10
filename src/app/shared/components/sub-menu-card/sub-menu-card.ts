1704e0d9e50c08ba6ffc3804fe47c3a824e09e5f
negin-khademi <negin.khademi@gmail.com>
2025-09-10 19:57:55 +0330

    feat: add divider to sub menu card

--- src/app/shared/components/sub-menu-card/sub-menu-card.ts
+++ src/app/shared/components/sub-menu-card/sub-menu-card.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { ButtonModule } from "primeng/button";
 import { CardModule } from "primeng/card";
+import { DividerModule } from "primeng/divider";
 
 @Component({
   selector: "app-sub-menu-card",
-  imports: [CardModule, ButtonModule],
+  imports: [CardModule, ButtonModule, DividerModule],
   templateUrl: "./sub-menu-card.html",
   styleUrl: "./sub-menu-card.scss",
 })
