ad7455fec1bae57ead4c6ab45c26b38db13d53ab
negin-khademi <negin.khademi@gmail.com>
2025-09-10 11:41:02 +0330

    feat : providing httpclient through independency injection

--- src/app/core/app.config.ts
+++ src/app/core/app.config.ts
@@ -4,14 +4,16 @@ import {
   provideZonelessChangeDetection,
 } from "@angular/core";
 
-import Aura from "@primeuix/themes/aura";
+import { provideHttpClient } from "@angular/common/http";
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
-import { providePrimeNG } from "primeng/config";
 import { provideRouter } from "@angular/router";
+import Aura from "@primeuix/themes/aura";
+import { providePrimeNG } from "primeng/config";
 import { routes } from "../app.routes";
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
