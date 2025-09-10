e20e7c0e15538758f02570d2d82a4c77722ea173
negin-khademi <negin.khademi@gmail.com>
2025-09-10 12:03:44 +0330

    feat : generate sib menu card

---
+++ src/app/shared/components/sub-menu-card/sub-menu-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sub-menu-card',
+  imports: [],
+  templateUrl: './sub-menu-card.html',
+  styleUrl: './sub-menu-card.scss'
+})
+export class SubMenuCard {
+
+}
