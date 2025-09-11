952f572e9fbf6887c9f8336a5722ca96c1083e4a
negin-khademi <negin.khademi@gmail.com>
2025-09-11 12:23:58 +0330

    feat : restaurant phone and logo ui

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
+import { Footer } from "../../layout/footer/footer";
 import { Header } from "../../layout/header/header";
 import { Hero } from "./hero/hero";
 import { MenuCards } from "./menu-cards/menu-cards";
 
 @Component({
   selector: "app-home",
-  imports: [Header, Hero, MenuCards],
+  imports: [Header, Hero, MenuCards, Footer],
   templateUrl: "./home.html",
   styleUrl: "./home.scss",
 })
