121bb6b8cbccefe76f3cb551f412604898b5bf79
negin-khademi <negin.khademi@gmail.com>
2025-09-15 14:15:55 +0330

    feat : add dark mode

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,12 @@ import { App } from "./app/app";
 import { appConfig } from "./app/core/app.config";
 import { bootstrapApplication } from "@angular/platform-browser";
 
+const root = document.documentElement;
+
+// Add 'dark' class if system prefers dark mode
+if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  root.classList.add("dark");
+} else {
+  root.classList.remove("dark");
+}
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
