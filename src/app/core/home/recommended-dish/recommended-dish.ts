a0d3a8de7eb63d89eadaa7bbb88f81317257c192
negin-khademi <negin.khademi@gmail.com>
2025-09-16 19:58:22 +0330

    feat : generate recommended dish component

---
+++ src/app/core/home/recommended-dish/recommended-dish.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recommended-dish',
+  imports: [],
+  templateUrl: './recommended-dish.html',
+  styleUrl: './recommended-dish.scss'
+})
+export class RecommendedDish {
+
+}
