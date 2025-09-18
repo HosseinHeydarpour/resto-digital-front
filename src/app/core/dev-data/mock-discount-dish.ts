b43f10b6e11045cc06de76f92f75fdeaa8eed9bb
negin-khademi <negin.khademi@gmail.com>
2025-09-18 16:12:17 +0330

    feat : generate mock data for discount dish

---
+++ src/app/core/dev-data/mock-discount-dish.ts
@@ -0,0 +1,46 @@
+import { IDisCountDish } from "../../shared/models/IDisCountDish";
+
+export const DiscountedRecommended: IDisCountDish[] = [
+  {
+    id: "f1",
+    name: "مرغ سوخاری",
+    price: 235000,
+    image: "images/recommended/big-chicken.png",
+    discount: 10,
+  },
+  {
+    id: "f2",
+    name: "مرغ تک نفره",
+    price: 142000,
+    image: "images/recommended/chicken.png",
+    discount: 15,
+  },
+  {
+    id: "f3",
+    name: "مرغ شکم پر",
+    price: 175000,
+    image: "images/recommended/dry-chicken.png",
+    discount: 8,
+  },
+  {
+    id: "f4",
+    name: "زرشک پلو",
+    price: 750000,
+    image: "images/recommended/kebeb.png",
+    discount: 12,
+  },
+  {
+    id: "f5",
+    name: "شامی",
+    price: 150000,
+    image: "images/recommended/shami.png",
+    discount: 5,
+  },
+  {
+    id: "f6",
+    name: "کتلت",
+    price: 65000,
+    image: "images/recommended/shami2.png",
+    discount: 20,
+  },
+];
