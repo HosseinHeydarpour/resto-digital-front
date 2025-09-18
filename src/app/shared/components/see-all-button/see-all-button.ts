22d5c695e78c0930abf34e603c256430e8c48e7e
negin-khademi <negin.khademi@gmail.com>
2025-09-18 13:57:49 +0330

    feat : generate see all button component

---
+++ src/app/shared/components/see-all-button/see-all-button.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-see-all-button',
+  imports: [],
+  templateUrl: './see-all-button.html',
+  styleUrl: './see-all-button.scss'
+})
+export class SeeAllButton {
+
+}
