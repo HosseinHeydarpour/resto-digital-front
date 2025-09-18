aab713c70bef9a99ace586233abfbf27ac050f66
negin-khademi <negin.khademi@gmail.com>
2025-09-18 16:14:54 +0330

    feat : generate discount dish component

---
+++ src/app/features/home/discount-dish/discount-dish.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-discount-dish',
+  imports: [],
+  templateUrl: './discount-dish.html',
+  styleUrl: './discount-dish.scss'
+})
+export class DiscountDish {
+
+}
