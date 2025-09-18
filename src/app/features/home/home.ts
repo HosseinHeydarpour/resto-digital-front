bef420b6a787e85645c7389d9e7af5b2c1f80a54
negin-khademi <negin.khademi@gmail.com>
2025-09-18 16:17:51 +0330

    feat : import discount component in home

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { DiscountDish } from "./discount-dish/discount-dish";
 import { Footer } from "../../layout/footer/footer";
 import { Header } from "../../layout/header/header";
 import { Hero } from "./hero/hero";
@@ -7,7 +8,7 @@ import { RecommendedDish } from "./recommended-dish/recommended-dish";
 
 @Component({
   selector: "app-home",
-  imports: [Header, Hero, MenuCards, Footer, RecommendedDish],
+  imports: [Header, Hero, MenuCards, Footer, RecommendedDish, DiscountDish],
   templateUrl: "./home.html",
   styleUrl: "./home.scss",
 })
