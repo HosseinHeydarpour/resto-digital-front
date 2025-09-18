bb3ddcbf483b1fa3d8f7c3a907fe7da035d28da4
negin-khademi <negin.khademi@gmail.com>
2025-09-18 12:20:54 +0330

    feat : import service wtich uses signal tech in component

--- src/app/features/home/recommended-dish/recommended-dish.ts
+++ src/app/features/home/recommended-dish/recommended-dish.ts
@@ -1,18 +1,12 @@
-import { Component, signal } from "@angular/core";
+import { Component, effect, inject, signal } from "@angular/core";
 
 import { ButtonModule } from "primeng/button";
 import { CarouselModule } from "primeng/carousel";
 import { CommonModule } from "@angular/common";
 import { Divider } from "../../../shared/components/divider/divider";
 import { IRecommendedDish } from "../../../shared/models/IRecommendedFood";
+import { RecommendedDishService } from "../../../core/services/recommed-dish-service";
 import { TagModule } from "primeng/tag";
-
-interface Car {
-  id?: string;
-  name?: string;
-  description?: string;
-  price?: number;
-}
 
 @Component({
   selector: "app-recommended-dish",
@@ -22,47 +16,18 @@ interface Car {
 })
 export class RecommendedDish {
   recommendedTitle = signal("پیشنهاد سرآشپز");
+  recDishService = inject(RecommendedDishService);
   recommendedDish: IRecommendedDish[] = [];
 
-  cars: Car[] = [];
   responsiveOptions: any[] | undefined;
 
-  constructor() {}
+  constructor() {
+    effect(() => {
+      this.recDishService.getRecDishes();
+    });
+  }
 
   ngOnInit() {
-    this.cars = [
-      {
-        id: "1",
-        name: "Bugatti",
-        description: "Racing car",
-        price: 800,
-      },
-      {
-        id: "2",
-        name: "Ferrari",
-        description: "The Prancing Horse",
-        price: 1500,
-      },
-      {
-        id: "3",
-        name: "Porsche",
-        description: "Full spectrum",
-        price: 10000,
-      },
-      {
-        id: "4",
-        name: "Ferrari",
-        description: "The Prancing Horse",
-        price: 1500,
-      },
-      {
-        id: "5",
-        name: "Porsche",
-        description: "Full spectrum",
-        price: 10000,
-      },
-    ];
-
     this.responsiveOptions = [
       {
         breakpoint: "1400px", // screen <= 1400px
