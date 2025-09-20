63931e076560bc400cb71d80776c28eb1bcce881
negin-khademi <negin.khademi@gmail.com>
2025-09-20 13:56:13 +0330

    feat : generate customers comments component

---
+++ src/app/features/home/customers-comments/customers-comments.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customers-comments',
+  imports: [],
+  templateUrl: './customers-comments.html',
+  styleUrl: './customers-comments.scss'
+})
+export class CustomersComments {
+
+}
