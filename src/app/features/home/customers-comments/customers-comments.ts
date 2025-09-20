d85cd7474200648a00e41385440f8612015362c2
negin-khademi <negin.khademi@gmail.com>
2025-09-20 22:39:24 +0330

    feat : ui and logic of comments component

--- src/app/features/home/customers-comments/customers-comments.ts
+++ src/app/features/home/customers-comments/customers-comments.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject, signal } from "@angular/core";
+
+import { Carousel } from "primeng/carousel";
+import { CommentsService } from "../../../core/services/comments-service";
+import { Divider } from "../../../shared/components/divider/divider";
+import { IComments } from "../../../shared/models/ICustomComment";
+import { TagModule } from "primeng/tag";
 
 @Component({
-  selector: 'app-customers-comments',
-  imports: [],
-  templateUrl: './customers-comments.html',
-  styleUrl: './customers-comments.scss'
+  selector: "app-customers-comments",
+  imports: [Carousel, Divider, TagModule],
+  templateUrl: "./customers-comments.html",
+  styleUrl: "./customers-comments.scss",
 })
 export class CustomersComments {
+  commentsTitle = signal("نظرات مشتریان");
+  responsiveOptions: any[] | undefined;
+  commentsService = inject(CommentsService);
+  comments: IComments[] = [];
 
+  constructor() {
+    effect(() => {
+      this.comments = this.commentsService.getAllComments();
+      console.log(this.comments);
+    });
+  }
+
+  ngOnInit() {
+    this.responsiveOptions = [
+      {
+        breakpoint: "1400px", // screen <= 1400px
+        numVisible: 3,
+        numScroll: 1,
+      },
+      {
+        breakpoint: "768px", // screen <= 768px
+        numVisible: 2,
+        numScroll: 1,
+      },
+      {
+        breakpoint: "426px", // screen <= 426px
+        numVisible: 1,
+        numScroll: 1,
+      },
+    ];
+  }
 }
