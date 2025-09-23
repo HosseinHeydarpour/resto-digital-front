4a00434681f1168ad10dac1eba7d8aac2dcc163b
negin-khademi <negin.khademi@gmail.com>
2025-09-23 16:33:53 +0330

    feat : ui contact card

--- src/app/shared/components/contact-card/contact-card.ts
+++ src/app/shared/components/contact-card/contact-card.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input, Signal } from "@angular/core";
+
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-contact-card',
+  selector: "app-contact-card",
   imports: [],
-  templateUrl: './contact-card.html',
-  styleUrl: './contact-card.scss'
+  templateUrl: "./contact-card.html",
+  styleUrl: "./contact-card.scss",
 })
 export class ContactCard {
+  @Input({ required: true }) icon!: Signal<string>;
+  @Input({ required: true }) title!: Signal<string>;
 
+  @Input() route?: Signal<string>; // for internal navigation
+  @Input({ required: true }) url?: Signal<string>; // for external links
+
+  constructor(private router: Router) {}
+
+  navigate() {
+    if (this.route) {
+      this.router.navigate([this.route]);
+    } else if (this.url) {
+      window.open(this.url(), "_blank");
+    }
+  }
 }
