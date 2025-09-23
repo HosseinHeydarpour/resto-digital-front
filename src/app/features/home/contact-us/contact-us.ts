8770abb48c02a46b4628ee567e9931b50fa294a7
negin-khademi <negin.khademi@gmail.com>
2025-09-23 16:34:41 +0330

    feat : use contact card in cantact us component and add important data

--- src/app/features/home/contact-us/contact-us.ts
+++ src/app/features/home/contact-us/contact-us.ts
@@ -1,10 +1,30 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
+
 import { ContactCard } from "../../../shared/components/contact-card/contact-card";
+import { Divider } from "../../../shared/components/divider/divider";
 
 @Component({
   selector: "app-contact-us",
-  imports: [ContactCard],
+  imports: [ContactCard, Divider],
   templateUrl: "./contact-us.html",
   styleUrl: "./contact-us.scss",
 })
-export class ContactUs {}
+export class ContactUs {
+  contactUsTitle = signal("راه های ارتباطی با ترنج");
+
+  telegramIcon = signal("pi pi-telegram");
+  telegramTitle = signal("تلگرام");
+  telegramUrl = signal("https://t.me/toranj");
+
+  whatsappIcon = signal("pi pi-whatsapp");
+  whatsappTitle = signal("واتساپ");
+  whatsappUrl = signal("https://wa.me/1234567890"); // put your number
+
+  instagramIcon = signal("pi pi-instagram");
+  instagramTitle = signal("اینستاگرام");
+  instagramUrl = signal("https://instagram.com/toranj");
+
+  xIcon = signal("pi pi-twitter"); // PrimeIcons doesn’t have "X", so we use twitter
+  xTitle = signal("ایکس");
+  xUrl = signal("https://x.com/toranj");
+}
