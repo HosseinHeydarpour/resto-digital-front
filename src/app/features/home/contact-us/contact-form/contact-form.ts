d71355f955f7b64e3df5951e35bebdd72486a405
negin-khademi <negin.khademi@gmail.com>
2025-09-27 10:36:51 +0330

    feat : create contact form and work on ui and logic

--- src/app/features/home/contact-us/contact-form/contact-form.ts
+++ src/app/features/home/contact-us/contact-form/contact-form.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { Select, SelectModule } from "primeng/select";
+
+import { Component } from "@angular/core";
+import { InputNumberModule } from "primeng/inputnumber";
+import { InputTextModule } from "primeng/inputtext";
+import { Message } from "primeng/message";
+import { TextareaModule } from "primeng/textarea";
+import { Toast } from "primeng/toast";
+
+interface RequestSubject {
+  name: string;
+  code: string;
+}
 
 @Component({
-  selector: 'app-contact-form',
-  imports: [],
-  templateUrl: './contact-form.html',
-  styleUrl: './contact-form.scss'
+  selector: "app-contact-form",
+  imports: [
+    ReactiveFormsModule,
+    InputTextModule,
+    InputNumberModule,
+    Select,
+    Message,
+    Toast,
+    SelectModule,
+    TextareaModule,
+  ],
+  templateUrl: "./contact-form.html",
+  styleUrl: "./contact-form.scss",
 })
 export class ContactForm {
+  requests: RequestSubject[] = [
+    { name: "انتقاد", code: "Feedback" },
+    { name: "پیشنهاد", code: "Suggestion" },
+  ];
 
+  contactForm = new FormGroup({
+    name: new FormControl("میلاد برزگر"),
+    phone: new FormControl(),
+    reqSubject: new FormControl("Feedback"),
+    message: new FormControl(""),
+    // city: ["", Validators.required],
+  });
+
+  formSubmit() {}
 }
