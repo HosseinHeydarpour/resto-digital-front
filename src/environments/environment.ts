9748ebd42c305c7d0fb8660e2d4b5e57121a18e9
negin-khademi <negin.khademi@gmail.com>
2025-09-30 22:16:06 +0330

    chore(env): add grouped API endpoints for users (signup & login)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,9 @@
 export const environment = {
   MAIN_SITE_URL: "https://resto-digital.onrender.com",
+  API: {
+    USERS: {
+      SIGNUP: "https://resto-digital.onrender.com/api/v1/users/signup",
+      LOGIN: "https://resto-digital.onrender.com/api/v1/users/login",
+    },
+  },
 };
