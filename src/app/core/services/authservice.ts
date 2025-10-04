0f3e78ab8b04bdbd59a5d732e192242d4c6c591d
negin-khademi <negin.khademi@gmail.com>
2025-10-04 11:54:43 +0330

    feat : create a method for login and signup

--- src/app/core/services/authservice.ts
+++ src/app/core/services/authservice.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment } from "../../../environments/environment";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class Authservice {
-  
+  constructor(private http: HttpClient) {}
+
+  registerUser(userData: any): Observable<any> {
+    // Use the SIGNUP URL directly from the environment
+    return this.http.post(environment.API.USERS.SIGNUP, userData);
+  }
+
+  loginUser(credentials: any): Observable<any> {
+    // Use the LOGIN URL directly from the environment
+    return this.http.post(environment.API.USERS.LOGIN, credentials);
+  }
 }
