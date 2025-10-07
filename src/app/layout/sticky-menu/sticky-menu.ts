4d21093a0baf2252b42757061aa82d746df69224
negin-khademi <negin.khademi@gmail.com>
2025-10-07 13:08:13 +0330

    feat : change dialog header based on the form

--- src/app/layout/sticky-menu/sticky-menu.ts
+++ src/app/layout/sticky-menu/sticky-menu.ts
@@ -1,3 +1,5 @@
+import { ActivatedRoute, Router } from "@angular/router";
+
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { Dialog } from "primeng/dialog";
@@ -11,7 +13,10 @@ import { RegisterLogin } from "../../features/auth/register-login/register-login
 })
 export class StickyMenu {
   activeLink: string = "home";
+  dialogHeader = "ایجاد حساب کاربری"; // default header
   displayLoginDialog: boolean = false;
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
 
   setActive(link: string) {
     this.activeLink = link;
@@ -20,5 +25,16 @@ export class StickyMenu {
   handleProfileClick(): void {
     this.setActive("profile");
     this.displayLoginDialog = true;
+  }
+
+  onModeChange(mode: "signup" | "signin") {
+    this.dialogHeader =
+      mode === "signup" ? "ایجاد حساب کاربری" : "ورود به حساب کاربری";
+
+    this.router.navigate([], {
+      relativeTo: this.activatedRoute,
+      queryParams: { mode: mode },
+      queryParamsHandling: "merge",
+    });
   }
 }
