ce9d9f1ee18944a65fcc38434b3e751e00f3e24d
negin-khademi <negin.khademi@gmail.com>
2025-10-07 12:54:53 +0330

    feat : show dialog in mobile mode when user click on profile button

--- src/app/layout/sticky-menu/sticky-menu.ts
+++ src/app/layout/sticky-menu/sticky-menu.ts
@@ -1,16 +1,24 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
+import { Dialog } from "primeng/dialog";
+import { RegisterLogin } from "../../features/auth/register-login/register-login";
 
 @Component({
   selector: "app-sticky-menu",
-  imports: [CommonModule],
+  imports: [CommonModule, Dialog, RegisterLogin],
   templateUrl: "./sticky-menu.html",
   styleUrl: "./sticky-menu.scss",
 })
 export class StickyMenu {
   activeLink: string = "home";
+  displayLoginDialog: boolean = false;
 
   setActive(link: string) {
     this.activeLink = link;
+  }
+
+  handleProfileClick(): void {
+    this.setActive("profile");
+    this.displayLoginDialog = true;
   }
 }
